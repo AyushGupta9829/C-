7fc0cbd972da240191ea43ff11dd0d781d8f5b65
thisisayush2912@gmail.com <thisisayush2912@gmail.com>
2021-08-23 15:50:33 +0530

    NEW

--- Program.cs
+++ Program.cs
@@ -5,13 +5,34 @@ namespace btod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome cSharp ");
-          
+            int i, fact = 1, n;
+            Console.WriteLine("Enter any Number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("The factorial is : " + fact);
+            
+
+            int n1 = 0, n2 = 1, n3, j, x;
+            Console.Write("Enter the number of elements: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            for (j = 2; j < x; ++j)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
             Console.ReadLine();
-
-
-
-
         }
+
+
+
+
+            
     }
 }
