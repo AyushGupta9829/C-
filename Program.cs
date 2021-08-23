7cd336c27f27ab5a2769edf5b7df4e92586c0536
thisisayush2912@gmail.com <thisisayush2912@gmail.com>
2021-08-23 13:03:18 +0530

    Doc

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace btod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome cSharp ");
+          
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
